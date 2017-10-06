02e71399c16a1a7bee78e5078e75d394d71f0181
aver0214 <31089929+aver0214@users.noreply.github.com>
2017-10-06 12:07:16 +0800

    Revert "[W7.7][F10-B4] update of user stories to developer guide"

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -6,7 +6,6 @@ import javafx.collections.ObservableList;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
-import seedu.address.model.tag.Tag;
 
 /**
  * The API of the Model component.
@@ -26,8 +25,6 @@ public interface Model {
 
     /** Adds the given person */
     void addPerson(ReadOnlyPerson person) throws DuplicatePersonException;
-
-    void deleteTag(Tag tag) throws PersonNotFoundException, DuplicatePersonException;
 
     /**
      * Replaces the given person {@code target} with {@code editedPerson}.
